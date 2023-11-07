2c762a941b7c38ede81dfd6b30adb86468c11f0c
Nikunj_Thakkar <nikunjthakkar1308@gmail.com>
2023-11-06 23:58:47 -0500

    Updated till 5th

--- test/src/edu/umb/cs680/hw05/KitchenTest.java
+++ test/src/edu/umb/cs680/hw05/KitchenTest.java
@@ -1,107 +1,126 @@
 package edu.umb.cs680.hw05;
 
 import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class KitchenTest {
 	@Test
 	public void testTemperatureEventConstructorAndGetterForTemp25() {
-		TemperatureEvent event = new TemperatureEvent(25);
-		int expected = 25;
-		int actual = event.getTemperature();
-		Assertions.assertEquals(expected, actual);
+		TemperatureEvent event = new TemperatureEvent(25); // Event Created
+		int expected = 25; // Expected Temperature
+		int actual = event.getTemperature(); // Initialize actual temperature with getTemperature method
+		Assertions.assertEquals(expected, actual); // Check if Constructor initializes correctly and same value is
+													// returned by getter method
 	}
 
 	@Test
 	public void testKitchenControllerIsObservable() {
 		KitchenControllerImpl controller = new KitchenControllerImpl();
-		Assertions.assertTrue(controller instanceof Observable);
+		Assertions.assertTrue(controller instanceof Observable); // Test if KitchenControllerImpl instance is of type
+																	// observable
 	}
 
 	@Test
 	public void testKitchenControllerIsNotObserver() {
 		KitchenControllerImpl controller = new KitchenControllerImpl();
-		Assertions.assertFalse(controller instanceof Observer);
+		Assertions.assertFalse(controller instanceof Observer); // Test if KitchenControllerImpl instance is not of type
+																// observer
 	}
 
 	@Test
-	public void testUpdateMethodForSmartFridge() {
+	public void testTemperatureChangeFrom25To30WithChangeTemperatureForFridge() {
 		KitchenControllerImpl controller = new KitchenControllerImpl();
-		SmartFridge fridge = new SmartFridge();
-		controller.registerDevice(fridge);
+		SmartFridge fridge = new SmartFridge(); // Device created
+		controller.registerDevice(fridge); // Device registered
 
-		Assertions.assertTrue(fridge instanceof Observer);
+		Assertions.assertTrue(fridge instanceof Observer); // Test if device created is of type observer
+		int expected = 30; // Expected Temperature
 
-		TemperatureEvent event = new TemperatureEvent(30);
-		controller.notifyObservers(event);
-	}
+		controller.changeTemperature(25);
+		controller.changeTemperature(30); // Change Temperature to 30
 
-	@Test
-	public void testUpdateMethodForSmartOven() {
-		KitchenControllerImpl controller = new KitchenControllerImpl();
-		SmartOven oven = new SmartOven();
-		controller.registerDevice(oven);
-
-		Assertions.assertTrue(oven instanceof Observer);
-
-		TemperatureEvent event = new TemperatureEvent(160);
-		controller.notifyObservers(event);
-	}
-
-	@Test
-	public void testRegisterDeviceInKitchenController() {
-		KitchenControllerImpl controller = new KitchenControllerImpl();
-		int expected = 1;
-		SmartFridge fridge = new SmartFridge();
-		controller.registerDevice(fridge);
-		int actual = controller.countObservers();
+		int actual = fridge.getFridgeLastTemperature(); // Retrieve Fridge Temp
 		Assertions.assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testNumberOfRegisterDeviceInKitchenController() {
+	public void testTemperatureChangeFrom25To30WithChangeTemperatureForOven() {
+		KitchenControllerImpl controller = new KitchenControllerImpl();
+		SmartOven oven = new SmartOven(); // Device created
+		controller.registerDevice(oven); // Device registered
+
+		Assertions.assertTrue(oven instanceof Observer); // Test if device created is of type observer
+		int expected = 30; // Expected Temperature
+
+		controller.changeTemperature(25); // Initial Temp
+		controller.changeTemperature(30); // Changed Temperature
+
+		int actual = oven.getOvenLastTemperature(); // Retrieve Oven Temp
+		Assertions.assertEquals(expected, actual); // Verify
+	}
+
+	@Test
+	public void testRegisterDeviceFridgeInKitchenController() {
+		KitchenControllerImpl controller = new KitchenControllerImpl();
+		int expected = 1;
+		SmartFridge fridge = new SmartFridge(); // Create one device
+		controller.registerDevice(fridge); // Register the device
+		int actual = controller.countObservers(); // Count number of devices
+		Assertions.assertEquals(expected, actual); // Verify
+	}
+
+	@Test
+	public void testRegisterDeviceOvenInKitchenController() {
+		KitchenControllerImpl controller = new KitchenControllerImpl();
+		int expected = 1;
+		SmartOven oven = new SmartOven(); // Create one device
+		controller.registerDevice(oven); // Register the device
+		int actual = controller.countObservers(); // Count number of devices
+		Assertions.assertEquals(expected, actual); // Verify
+	}
+
+	@Test
+	public void testNumberOfRegisterDeviceInKitchenControllerFridgeAndOven() {
 		KitchenControllerImpl controller = new KitchenControllerImpl();
 
 		int expected = 1;
 
-		SmartFridge fridge = new SmartFridge();
-		controller.registerDevice(fridge);
+		SmartFridge fridge = new SmartFridge(); // Create one device
+		controller.registerDevice(fridge); // Register the device
 		int actual = controller.countObservers();
 
 		Assertions.assertEquals(expected, actual);
 
-		SmartOven oven = new SmartOven();
-		controller.registerDevice(oven);
+		SmartOven oven = new SmartOven(); // Create another device
+		controller.registerDevice(oven); // Register the device
 		actual = controller.countObservers();
 
 		expected++;
-		Assertions.assertEquals(expected, actual);
+		Assertions.assertEquals(expected, actual); // Verify device count
 	}
 
 	@Test
 	public void testFridgeOvenFridgeRegisterDeviceInKitchenController() {
 		KitchenControllerImpl controller = new KitchenControllerImpl();
 
-		SmartFridge fridge_1 = new SmartFridge();
-		controller.registerDevice(fridge_1);
+		SmartFridge fridge_1 = new SmartFridge(); // Create one device
+		controller.registerDevice(fridge_1); // Register the device
 
-		SmartOven oven = new SmartOven();
-		controller.registerDevice(oven);
+		SmartOven oven = new SmartOven(); // Create another device
+		controller.registerDevice(oven); // Register the device
 
-		SmartFridge fridge_2 = new SmartFridge();
-		controller.registerDevice(fridge_2);
+		SmartFridge fridge_2 = new SmartFridge(); // Create another device
+		controller.registerDevice(fridge_2); // Register the device
 
 		List<Observer<TemperatureEvent>> expected = new ArrayList<>();
 		expected.add(fridge_1);
 		expected.add(oven);
-		expected.add(fridge_2);
+		expected.add(fridge_2); // List of devices prepared
 
-		List<Observer<TemperatureEvent>> actual = controller.getObservers();
+		List<Observer<TemperatureEvent>> actual = controller.getObservers(); // Get the list of devices
 
-		Assertions.assertIterableEquals(expected, actual);
+		Assertions.assertIterableEquals(expected, actual); // Verify list of devices registered
 	}
 
 	@Test
@@ -109,10 +128,10 @@ public class KitchenTest {
 		int expected = 30;
 		KitchenControllerImpl controller = new KitchenControllerImpl();
 
-		SmartFridge fridge = new SmartFridge();
+		SmartFridge fridge = new SmartFridge(); // Create one device
 
-		controller.registerDevice(fridge);
-		controller.changeTemperature(30);
+		controller.registerDevice(fridge); // Register the device
+		controller.changeTemperature(30); // Change Temperature
 
 		Assertions.assertEquals(expected, fridge.getFridgeLastTemperature());
 	}
@@ -122,10 +141,25 @@ public class KitchenTest {
 		int expected = 30;
 		KitchenControllerImpl controller = new KitchenControllerImpl();
 
-		SmartOven oven = new SmartOven();
+		SmartOven oven = new SmartOven(); // Create one device
 
-		controller.registerDevice(oven);
-		controller.changeTemperature(30);
+		controller.registerDevice(oven); // Register the device
+		controller.changeTemperature(30); // Change Temperature
+
+		Assertions.assertEquals(expected, oven.getOvenLastTemperature());
+	}
+
+	@Test
+	public void testTemperatureChange30AfterFridgeRegisterEventForOven() {
+		int expected = 30;
+		KitchenControllerImpl controller = new KitchenControllerImpl();
+
+		SmartOven oven = new SmartOven(); // Create one device
+
+		controller.registerDevice(oven); // Register the device
+		SmartFridge fridge = new SmartFridge(); // Create one device
+		controller.registerDevice(fridge); // Register the device
+		controller.changeTemperature(30); // Change Temperature
 
 		Assertions.assertEquals(expected, oven.getOvenLastTemperature());
 	}
@@ -135,20 +169,19 @@ public class KitchenTest {
 		int expected = 30;
 		KitchenControllerImpl controller = new KitchenControllerImpl();
 
-		SmartFridge fridge = new SmartFridge();
-		SmartOven oven = new SmartOven();
+		SmartFridge fridge = new SmartFridge(); // Create one device
+		SmartOven oven = new SmartOven(); // Create one device
 
-		controller.registerDevice(fridge);
-		controller.registerDevice(oven);
-		controller.changeTemperature(30);
+		controller.registerDevice(fridge); // Register the device
+		controller.registerDevice(oven); // Register the device
+		controller.changeTemperature(30); // Change Temperature
 
 		Assertions.assertEquals(expected, fridge.getFridgeLastTemperature());
 		Assertions.assertEquals(expected, oven.getOvenLastTemperature());
 
-		controller.changeTemperature(80);
+		controller.changeTemperature(80); // Change Temperature
 		expected = 80;
 		Assertions.assertEquals(expected, fridge.getFridgeLastTemperature());
 		Assertions.assertEquals(expected, oven.getOvenLastTemperature());
 	}
-
 }
